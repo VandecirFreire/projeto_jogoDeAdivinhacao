03f24280e9b1a8e1cee558cef52d0faa30d03e4b
Vandecir <15702156+VandecirFreire@users.noreply.github.com>
2023-01-18 14:13:52 -0300

    refatorando

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-const screen1 = document.querySelector(".screen1")
-const screen2 = document.querySelector(".screen2")
-const randomNumber = Math.round(Math.random() * 10); // gerar numero randômico
+// Variáveis
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+let randomNumber = Math.round(Math.random() * 10); // gerar numero randômico
 let xAttempts = 1
 
+// Eventos
+btnTry.addEventListener('click', handleTryClick);
+btnReset.addEventListener('click', handResetClick);
+document.addEventListener('keydown', function(e) {
+  if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handResetClick()
+  }
+});
 
-// função callback = é uma funcao que é passada como argumento de uma outra funcao e essa funcao vai ser executada depois
+// funções de callback = é uma funcao que é passada como argumento de uma outra funcao e essa funcao vai ser executada depois
 function handleTryClick(event) {
   event.preventDefault(); // nao faça o padrao desse evento, que no caso é o form
 
   const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide")
-    screen2.classList.remove("hide")
-
+    toggleScreen()
     document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas!`
     //screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas!` - tbm pode ser usado assim
   }
@@ -22,13 +31,13 @@ function handleTryClick(event) {
   xAttempts++
 }
 
-// Eventos
-const btnTry = document.querySelector("#btnTry");
-const btnReset = document.querySelector("#btnReset");
-
-btnTry.addEventListener('click', handleTryClick);
-btnReset.addEventListener('click', function () {
-  screen1.classList.remove("hide")
-  screen2.classList.add("hide")
+function handResetClick() {
+  toggleScreen()
   xAttempts = 1
-})
+  randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
